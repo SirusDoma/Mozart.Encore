183129bb00de7aa8276e4a102f573e39969b0294
CXO2 <com@cxo2.me>
2025-08-31 09:41:37 +0900

    Set HTTP Options `Enabled` default to `false`
    There's no HTTP element in v3.10 by default.

--- Source/Mozart.Data/Options/HttpOptions.cs
+++ Source/Mozart.Data/Options/HttpOptions.cs
@@ -4,7 +4,7 @@ public sealed class HttpOptions
 {
     public const string Section = "Http";
 
-    public bool Enabled { get; init; } = true;
+    public bool Enabled { get; init; } = false;
     public string Address { get; init; } = "127.0.0.1";
     public int Port { get; init; } = 15000;
 }
