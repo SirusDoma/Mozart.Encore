cdb13d515701869031587bac8cf1eac8d2a332e9
CXO2 <com@cxo2.me>
2026-04-17 03:20:20 +0900

    Bump version

--- Source/Identity/Program.cs
+++ Source/Identity/Program.cs
@@ -30,7 +30,7 @@ namespace Identity;
 
 public class Program
 {
-    public static Version Version        => new(4, 2, 0);
+    public static Version Version        => new(4, 3, 0);
     public static Version NetworkVersion => new(5, 89);
     public static string RepositoryUrl   => "https://github.com/SirusDoma/Mozart.Encore";
 
