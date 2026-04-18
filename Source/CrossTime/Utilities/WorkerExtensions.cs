5a7f5e5c1c69cd86cabbb2b371e25e0939c569de
CXO2 <com@cxo2.me>
2026-04-18 15:14:11 +0900

    Sync with main branch

--- Source/CrossTime/Utilities/WorkerExtensions.cs
+++ Source/CrossTime/Utilities/WorkerExtensions.cs
@@ -9,23 +9,12 @@ public static class WorkerExtensions
     public static void ValidateMetadata(this IHostedService service, IChannelService channelService,
         IMetadataResolver resolver, ILogger logger)
     {
-        foreach (var channel in channelService.GetChannels())
-        {
-            try
-            {
-                _ = resolver.GetMusicList(channel);
-            }
-            catch (Exception ex)
-            {
-                logger.LogWarning(ex, "Failed to validate music list file");
-            }
-        }
 
         foreach (var channel in channelService.GetChannels())
         {
             try
             {
-                _ = resolver.GetAlbumList(channel);
+                _ = resolver.GetMusicList(channel);
             }
             catch (Exception ex)
             {
